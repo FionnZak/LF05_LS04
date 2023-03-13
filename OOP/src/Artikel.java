ae9f209d2a6509baf46d52c082d9ca96ba83cdd1
Fionn <fionn.zak@zalando.de>
2023-03-13 14:58:10 +0100

    Test

---
+++ OOP/src/Artikel.java
@@ -0,0 +1,50 @@
+public class Artikel {
+
+	// Attribute
+
+	private String bezeichnung;
+	private int nummer;
+	private double einkaufspreisInEuro;
+	private double verkaufspreisInEuro;
+	private int sollBestand;
+	private int istBestand;
+	// Methoden
+
+	public Artikel() {
+
+	}
+
+	public void bestellen() {
+		if sollBestand == istBestand 
+	}
+
+	public double gewinnBerechnen() {
+		return einkaufspreisInEuro - verkaufspreisInEuro;
+	}
+
+	public String getBezeichnung(){
+        this.bezeichnung;
+    }
+
+	public int getNummer() {
+		this.nummer();
+	}
+
+	public double getEinkaufspreisInEuro(){
+        this.einkaufspreisInEuro;
+    }
+
+	public double getVerkaufspreisInEuro(){
+        this.verkaufspreisInEuro;
+    }
+
+	public int getSollBestand(){
+        this.sollBestand;
+    }
+
+	public int getIstBestand(){
+        this.istBestand;
+    }
+
+	public setBezeichnung
+}
